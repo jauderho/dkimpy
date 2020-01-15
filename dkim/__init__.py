7a5615cec9a22e126f6f443376d339d1b1d33635
Scott Kitterman <scott@kitterman.com>
2020-01-15 11:03:07 -0500

    - Correct signature indexing error introduced in 1.0.0 that prevents       verification of multiple signatures in a single message

--- dkim/__init__.py
+++ dkim/__init__.py
@@ -934,7 +934,7 @@ class DKIM(DomainSigner):
   #: @return: True if signature verifies or False otherwise
   #: @raise DKIMException: when the message, signature, or key are badly formed
   def verify(self,idx=0,dnsfunc=get_txt):
-    sig, include_headers, sigheaders = self.verify_headerprep(idx=0)
+    sig, include_headers, sigheaders = self.verify_headerprep(idx)
     return self.verify_sig(sig, include_headers, sigheaders[idx], dnsfunc)
 
 
